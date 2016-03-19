0379b321fea22b920a15802237f4db29bf687d6f
hxghxg527 <hxghxg527@163.com>
2016-03-19 21:08:40 +0800

    update.

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 /**
  * Created by hxghxg527 on 16/3/19.
  */
+'use strict';
 
-var express = require('http');
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    response.end('hello world!!');
+}).listen(8080);
+
+console.log('Server running at http://localhost:8080/');
---
+++ src/javascript/main.js
@@ -0,0 +1,4 @@
+/**
+ * Created by hxghxg527 on 16/3/19.
+ */
+'use strict';

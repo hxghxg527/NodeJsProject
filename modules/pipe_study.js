63c7ad143ffcfdba4eef8eb6d1fec9f8792d5fdb
hxghxg527 <hxghxg527@163.com>
2016-04-16 22:45:23 +0800

    study pipe read resources.

---
+++ modules/pipe_study.js
@@ -0,0 +1,17 @@
+/**
+ * Created by hxghxg527 on 16/4/16.
+ */
+
+var http = require('http');
+var fs = require('fs');
+var request = require('request');
+
+//读取别的网站的资源，然后返回给response
+
+var app = http.createServer(function (req, res) {
+    request('http://imgcache.gtimg.cn/tencentvideo_v1/vstyle/web/v3/style/images/logo.png').pipe(res);
+});
+
+app.listen(8080);
+
+console.log('服务器监听在8080端口...');
